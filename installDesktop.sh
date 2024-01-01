7f603223878c931adac348241cb1d03cddcaac70
Dan <221143711@student.uj.ac.za>
2024-01-01 22:30:12 +0200

    Added more features to desktop script

--- installDesktop.sh
+++ installDesktop.sh
@@ -2,3 +2,9 @@
 
 # This script clones the freeBSD branches of my suckless repos, it also installs some other desktop tools
 
+# Install sound system from OpenBSD
+pkg install sndio
+sysrc sndiod_enable="YES"
+
+# Install fonts used
+pkg install intel-one-mono font-awesome b612
