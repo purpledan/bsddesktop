c4ca3439cb12d2b6c743ca4790fd476d5bb25274
Dan <221143711@student.uj.ac.za>
2024-01-03 09:46:44 +0200

    Added harfbuzz for st and auto-font installer

--- installDesktop.sh
+++ installDesktop.sh
@@ -7,4 +7,10 @@ pkg install sndio
 sysrc sndiod_enable="YES"
 
 # Install fonts used
-pkg install intel-one-mono font-awesome b612
+pkg install intel-one-mono b612 harfbuzz
+
+mkdir -p /usr/local/share/fonts/nerd-fonts/Go-Mono
+cd /usr/local/share/fonts/nerd-fonts/Go-Mono
+curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Go-Mono/Regular/GoMonoNerdFont-Regular.ttf
+
+#TODO: Automate activation of true type fonts in Xorg
