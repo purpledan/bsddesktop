d7a7ce0f8c84627747e77bd054f438568d08d5b9
Daniel <221143711@student.uj.ac.za>
2024-01-07 11:12:06 +0200

    Auto downloads required fonts

--- installDesktop.sh
+++ installDesktop.sh
@@ -9,8 +9,11 @@ sysrc sndiod_enable="YES"
 # Install fonts used
 pkg install intel-one-mono b612 harfbuzz
 
+# This pulls in the Go-Mono font from nerd-fonts
+# TODO: Should be updated to pull in intel-one-mono once it is patched
 mkdir -p /usr/local/share/fonts/nerd-fonts/Go-Mono
 cd /usr/local/share/fonts/nerd-fonts/Go-Mono
 curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Go-Mono/Regular/GoMonoNerdFont-Regular.ttf
 
 #TODO: Automate activation of true type fonts in Xorg
+
